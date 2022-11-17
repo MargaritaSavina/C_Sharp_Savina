90966e47890b8b0ea0ee8a55bdb92462cb6fb99d
Margarita <savmargo98@gmail.com>
2022-11-17 21:46:15 +0800

    001

---
+++ Seminar1/001/Program.cs
@@ -0,0 +1,6 @@
+﻿// вводится число, вывести его квадрат
+Console.WriteLine("Введите число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int square = namber * namber;
+Console.WriteLine("Ответ: ");
+Console.WriteLine(square);
