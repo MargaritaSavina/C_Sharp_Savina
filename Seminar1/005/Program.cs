765e026656ebec859577f4aaaf96c9a24435ce8c
Margarita <savmargo98@gmail.com>
2022-11-18 16:21:58 +0800

    Seminar1/005

---
+++ Seminar1/005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу, которая на вход принимает одно число, а на выоде выдает все целые числа от -N до N
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+int i = -N;
+while (i < N+1)
+{
+    Console.Write(i);
+    i++;
+}
