c14b25f6f6c1ac45356cc69507919043e58e027f
Margarita <savmargo98@gmail.com>
2022-11-18 13:05:38 +0800

    Seminar1/002

---
+++ Seminar1/002/Program.cs
@@ -0,0 +1,14 @@
+﻿// на вход поступает 2 числа, программа проверяет, являяется ли одно квадратом другого
+Console.WriteLine("Введите число 1: ");
+int namber1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число 2: ");
+int namber2 = Convert.ToInt32(Console.ReadLine()); 
+int square1 = namber1 * namber1;
+if (square1 == namber2) 
+{
+   Console.WriteLine($"Число {namber1} является квадратом числа {namber2} "); 
+}
+else
+{
+    Console.WriteLine($"Число {namber1} не является квадратом числа {namber2} ");
+} 
