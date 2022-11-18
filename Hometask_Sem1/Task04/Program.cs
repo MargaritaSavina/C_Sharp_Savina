5f47e16538c7876cc26e83103324a3365d9b52ce
Margarita <savmargo98@gmail.com>
2022-11-18 14:33:27 +0800

    Hometask_Sem1/Task04

---
+++ Hometask_Sem1/Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число 1: ");
+int namber1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число 2: ");
+int namber2 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число 3: ");
+int namber3 = Convert.ToInt32(Console.ReadLine()); 
+int max = namber1;
+if (max < namber2) max = namber2;
+else if (max < namber3) max = namber3;
+Console.Write("max =  ");
+Console.WriteLine(max);
