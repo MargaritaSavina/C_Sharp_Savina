4f617ec433350f517aa19aa6ac9da7ea19a67138
Margarita <savmargo98@gmail.com>
2022-11-18 13:58:15 +0800

    Seminar1/003

---
+++ Seminar1/003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа будет выдавать день недели по заданному номеру
+Console.WriteLine("Введите число от 1 до 7: ");
+int DayWeek = Convert.ToInt32(Console.ReadLine());
+if (DayWeek == 1 ) Console.WriteLine("Понедельник ");
+if (DayWeek == 2) Console.WriteLine("Вторник ");
+if (DayWeek == 3) Console.WriteLine("Спеда ");
+if (DayWeek == 4) Console.WriteLine("Четверг ");
+if (DayWeek == 5) Console.WriteLine("Пятница ");
+if (DayWeek == 6) Console.WriteLine("Суббота ");
+if (DayWeek == 7) Console.WriteLine("Воскресенье ");
+if (DayWeek < 1 || DayWeek > 7) Console.WriteLine("Ошибка ввода ");
