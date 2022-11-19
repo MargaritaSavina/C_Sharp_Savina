897cc37766dfebc17eeee650fa1754c2db81af45
Margarita <savmargo98@gmail.com>
2022-11-20 00:03:40 +0800

    Hometask_Sem2

---
+++ Hometask_Sem2/Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+Console.WriteLine("Введите  трехначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Введено не трехзначное число ");
+}
+else 
+{
+int result = number % 100;
+int SecondNumber = result / 10;
+Console.WriteLine(SecondNumber);
+}
---
+++ Hometask_Sem2/Task13/Program.cs
@@ -0,0 +1,71 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).2147483648 || number > 2147483647
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result;
+if (number < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+
+ else if (number > 99 && number < 1000)
+{
+result = number % 10;
+Console.WriteLine(result);
+}
+
+
+ else if (number > 999 && number < 10000)
+{
+result = number % 100;
+result = result / 10;
+Console.WriteLine(result);
+}
+
+else if (number > 9999 && number < 100000)
+{
+result = number % 1000;
+result = result / 100;
+Console.WriteLine(result);
+}
+
+
+else if (number > 99999 && number < 1000000)
+{
+result = number % 10000;
+result = result / 1000;
+Console.WriteLine(result);
+}
+
+else if (number > 999999 && number < 10000000)
+{
+result = number % 100000;
+result = result / 10000;
+Console.WriteLine(result);
+}
+
+
+else if (number > 9999999 && number < 100000000)
+{
+result = number % 1000000;
+result = result / 100000;
+Console.WriteLine(result);
+}
+
+
+else if (number > 99999999 && number < 1000000000)
+{
+result = number % 10000000;
+result = result / 1000000;
+Console.WriteLine(result);
+}
+
+
+else if (number > 999999999 && number < 2147483647)
+{
+result = number % 100000000;
+result = result / 10000000;
+Console.WriteLine(result);
+}
+
+
