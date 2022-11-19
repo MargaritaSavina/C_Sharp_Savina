06694939e447b4fb56e15f15a3f47014e2e78c8f
Margarita <savmargo98@gmail.com>
2022-11-19 20:31:43 +0800

    Errors corrected

--- Hometask_Sem1/Task04/Program.cs
+++ Hometask_Sem1/Task04/Program.cs
@@ -7,6 +7,6 @@ Console.WriteLine("Введите число 3: ");
 int number3 = Convert.ToInt32(Console.ReadLine()); 
 int max = number1;
 if (max < number2) max = number2;
-else if (max < number3) max = number3;
+if (max < number3) max = number3;
 Console.Write("max =  ");
 Console.WriteLine(max);
--- Hometask_Sem1/Task08/Program.cs
+++ Hometask_Sem1/Task08/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int i = 0;
-while (i < N)
+while (i < N-1)
 {
     i += 2;
     Console.WriteLine(i);
