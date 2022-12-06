511a5db19f84d24912d296f6dec5d6053b2b0743
Margarita <savmargo98@gmail.com>
2022-12-06 18:41:45 +0800

    Hometask_Sem6/

---
+++ Hometask_Sem6/task41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] EnterArray(int s)// int s - размер массива
+{
+    double[]arr = new double[s];
+    Console.WriteLine("Введите массив: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        
+        int num = Convert.ToInt32(Console.ReadLine());
+        arr[i] = num;
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]+",");
+    }
+}
+double[] array = EnterArray(size);
+PrintArray(array);
+double number = Positive(array);
+Console.Write("  ->  "+ number);
+
+ double Positive(double[] arr)
+ {
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > 0) count++;
+        }
+    return count;
+ }
---
+++ Hometask_Sem6/task43/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+int ratios = 4;
+double[] EnterArray(int size)
+{
+    double[]arr = new double[size];
+    Console.WriteLine("Введите коэффициенты: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        arr[i] = num;
+    }
+    return arr;
+}
+
+double[] array = EnterArray(ratios);
+if (array[2] == 0 && array[0] == 0) Console.WriteLine("Решения нет ");
+double[] Equation(double[] arr)
+{
+    double[] arrXY = new double[2];
+    // arr[0] = k1;       arr[1] = b1;
+    // arr[2] = k2;       arr[3] = b2;
+    double x = (arr[1] - arr[3]) / (arr[2] - arr[0]); 
+    double y = arr[0] * x + arr[3]; 
+    arrXY[0] = x;
+    arrXY[1] = y;       
+    return arrXY;       
+}
+double[] arrayXY = Equation(array);
+void PrintArray(double[] arr)
+{
+    Console.Write("("+arr[0]+";" + arr[1]+ ")");
+}
+PrintArray(arrayXY);
